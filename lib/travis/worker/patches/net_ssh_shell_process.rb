8b8ee1fd90047a3f4e67732a29f172834b3af76a
Sven Fuchs <svenfuchs@artweb-design.de>
2011-06-28 10:51:18 +0200

    add a comment to our monkeypatch for Net::SSH::Shell

--- lib/travis/worker/patches/net_ssh_shell_process.rb
+++ lib/travis/worker/patches/net_ssh_shell_process.rb
@@ -1,3 +1,6 @@
+# can be removed once v0.2.0 is available
+# see https://github.com/mitchellh/net-ssh-shell/commit/1ebd62670b77b5834f0c6dd1a4508cb881a3ed64
+
 Net::SSH::Shell::Process.class_eval do
   def on_stdout(ch, data)
     # if data.strip =~ /^#{manager.separator} (\d+)$/
