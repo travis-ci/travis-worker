b28a7145187294a2367a2519db9e637b2ae279c4
Sven Fuchs <svenfuchs@artweb-design.de>
2011-06-30 14:18:23 +0200

    make execute accept strings and symbols as config keys for :timeout

--- lib/travis/worker/shell/session.rb
+++ lib/travis/worker/shell/session.rb
@@ -51,7 +51,7 @@ module Travis
         end
 
         def execute(command, options = {})
-          timeout = options[:timeout].is_a?(Symbol) ? Travis::Worker.config.timeouts[options[:timeout]] : options[:timeout]
+          timeout = options[:timeout].is_a?(Numeric) ? options[:timeout] : Travis::Worker.config.timeouts[options[:timeout]]
           command = timetrap(command, :timeout => timeout) if options[:timeout]
           command = echoize(command) unless options[:echo] == false
           exec(command) { |p, data| buffer << data } == 0
