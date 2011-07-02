22adfd9d94ac0ecbdc10ba31a076f6e48ce9eb1c
Michael S. Klishin <michael@novemberain.com>
2011-07-02 16:57:00 +0400

    Correct what seems to be a typo

--- lib/travis/worker/cli.rb
+++ lib/travis/worker/cli.rb
@@ -3,6 +3,7 @@ require "thor"
 require "travis/worker"
 require "travis/worker/application"
 
+
 module Travis
   module Worker
 
@@ -12,7 +13,7 @@ module Travis
       def start
         config     = Travis::Worker.config
         dispatcher = Application.new(config)
-        dispatcher.bind(config.amqp)
+        dispatcher.bind(config.amqp.to_hash.deep_symbolize_keys)
       end
 
 
