428d53a8cbdabd6861085f65442ac591dd5c69e7
Michael S. Klishin <michael@novemberain.com>
2011-07-03 22:25:50 +0400

    Extract base worker class

---
+++ lib/travis/worker/workers/base.rb
@@ -0,0 +1,51 @@
+module Travis
+  module Worker
+    module Workers
+
+      class Base
+
+        #
+        # API
+        #
+
+        attr_reader :payload, :job, :reporter
+
+        def initialize(payload)
+          @payload  = payload.deep_symbolize_keys
+          @job      = job_type.new(payload)
+          @reporter = Reporter::Http.new(job.build)
+
+          job.observers << reporter
+        rescue VmNotFound, Errno::ECONNREFUSED
+          puts "#{$!.class.name}: #{$!.message}", $@
+          puts 'Can not connect to VM. Stopping job processing ...'
+          stop_processing
+          requeue
+          raise $!
+        end
+
+        def work!
+          # reporter.deliver_messages!
+          job.work!
+        rescue
+          puts "#{$!.class.name}: #{$!.message}", $@
+        ensure
+          sleep(0.1) until reporter.finished?
+        end
+
+        def job_type
+          payload.key?(:build) && payload[:build].key?(:config) ? Job::Build : Job::Config
+        end
+
+        def stop_processing
+          raise NotImplementedError
+        end
+
+        def requeue
+          raise NotImplementedError
+        end
+
+      end # Base
+    end # Workers
+  end # Worker
+end # Travis
--- lib/travis/worker/workers/resque.rb
+++ lib/travis/worker/workers/resque.rb
@@ -1,3 +1,5 @@
+require "travis/worker/workers/base"
+
 module Travis
   module Worker
     module Workers
@@ -6,34 +8,11 @@ module Travis
       #
       # The Worker instantiates jobs (currently based on the payload, should be based on
       # the queue) and runs them.
-      class Resque
-        attr_reader :payload, :job, :reporter
+      class Resque < Workers::Base
 
-        def initialize(payload)
-          @payload  = payload.deep_symbolize_keys
-          @job      = job_type.new(payload)
-          @reporter = Reporter::Http.new(job.build)
-          job.observers << reporter
-        rescue VmNotFound, Errno::ECONNREFUSED
-          puts "#{$!.class.name}: #{$!.message}", $@
-          puts 'Can not connect to VM. Stopping job processing ...'
-          stop_processing
-          requeue
-          raise $!
-        end
-
-        def work!
-          reporter.deliver_messages!
-          job.work!
-        rescue
-          puts "#{$!.class.name}: #{$!.message}", $@
-        ensure
-          sleep(0.1) until reporter.finished?
-        end
-
-        def job_type
-          payload.key?(:build) && payload[:build].key?(:config) ? Job::Build : Job::Config
-        end
+        #
+        # API
+        #
 
         def stop_processing
           Process.kill('USR2', Process.ppid)
@@ -42,7 +21,7 @@ module Travis
         def requeue
           Travis::Worker.class_eval { @queue = 'builds' }
           Resque.enqueue(Travis::Worker, payload)
-        end
+        end # requeue
       end # Resque
     end # Workers
   end # Worker
