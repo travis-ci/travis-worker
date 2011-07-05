af5fc8057989916698ff8f2a5129ce0ec782e105
Michael S. Klishin <michael@novemberain.com>
2011-07-05 10:24:19 +0400

    symboliz_keys => symbolize_keys

---
+++ lib/travis/worker/core_ext/ruby/hash/deep_symbolize_keys.rb
@@ -0,0 +1,9 @@
+class Hash
+  def deep_symbolize_keys
+    inject({}) { |result, (key, value)|
+      value = value.deep_symbolize_keys if value.is_a?(Hash)
+      result[(key.to_sym rescue key) || key] = value
+      result
+    }
+  end unless Hash.method_defined?(:deep_symbolize_keys)
+end
