b0e9c8ec6c2d73fdc345ed51209c840de3acc482
Sven Fuchs <svenfuchs@artweb-design.de>
2011-07-11 13:15:51 +0200

    don't try to restore missing snapshots. always restore to a snapshot before deleting it. add moar logging

--- lib/travis/worker/shell/session.rb
+++ lib/travis/worker/shell/session.rb
@@ -109,62 +109,74 @@ module Travis
         end
 
         def start_sandbox
-          puts '[vbox] Creating vbox snapshot ...'
           vbox_take_snapshot
-          puts '[vbox] Created.'
         end
 
         def rollback_sandbox
-          puts '[vbox] Rolling back to vbox snapshot ...'
           vbox_power_off
-          vbox_restore_snapshot
           vbox_delete_snapshots
           vbox_start_vm
-          puts '[vbox] Rolled back.'
         rescue
           puts "#{$!.class.name}: #{$!.message}", $@
         end
 
         def vbox_take_snapshot
+          puts "[vbox] Taking snapshot on #{vm_name}"
           vbox_manage "snapshot '#{vm_name}' take '#{vm_name}-sandbox'", :wait => "showvminfo '#{vm_name}' | grep #{vm_name}-sandbox"
+          puts "[vbox] Taken."
         end
 
         def vbox_power_off
+          puts "[vbox] Powering off #{vm_name} ..."
           vbox_manage "controlvm '#{vm_name}' poweroff", :wait => "showvminfo '#{vm_name}' | grep State | grep 'powered off'"
+          puts "[vbox] Powered off."
         end
 
         def vbox_restore_snapshot
+          puts "[vbox] Restoring current snapshot ..."
           vbox_manage "snapshot '#{vm_name}' restorecurrent"
+          puts "[vbox] Restored."
         end
 
         def vbox_delete_snapshots
           vbox_snapshots.reverse.each do |snapshot|
-            puts "[vbox] Deleting snapshot #{snapshot}..."
+            vbox_restore_snapshot
+            puts "[vbox] Deleting snapshot #{snapshot} ..."
             vbox_manage "snapshot '#{vm_name}' delete '#{snapshot}'"
             puts "[vbox] Deleted."
           end
         end
 
         def vbox_start_vm
+          puts "[vbox] Starting #{vm_name} ..."
           vbox_manage "startvm --type headless '#{vm_name}'", :wait => "showvminfo '#{vm_name}' | grep State | grep 'running'"
+          puts "[vbox] Started."
         end
 
         def vbox_manage(cmd, options = {})
           cmd = "VBoxManage #{cmd} >> #{log} 2>&1"
-          puts "[vbox] #{cmd}"
+          puts "[vbox] #{cmd}" unless options[:eval]
           system(cmd).tap do |result|
             raise "[vbox] #{cmd} failed. See #{log} for more information." unless result || options[:eval]
             vbox_wait(options[:wait]) if options[:wait]
           end
         end
 
+        def vbox_eval(cmd)
+          system "VBoxManage #{cmd} > /dev/null 2>&1"
+        end
+
         def vbox_wait(cmd)
-          sleep(0.5) until vbox_manage(cmd, :eval => true)
+          sleep(0.5) until vbox_eval(cmd)
         end
 
         def vbox_snapshots
           info = `vboxmanage showvminfo #{vm_name} --details`
-          info.split(/^Snapshots\s*/).last.split("\n").map { |line| line =~ /\(UUID: ([^\)]*)\)/ and $1 }.compact
+          if info =~ /^Snapshots\s*/
+            info.split(/^Snapshots\s*/).last.split("\n").map { |line| line =~ /\(UUID: ([^\)]*)\)/ and $1 }.compact
+          else
+            []
+          end
         end
       end # Session
     end # Shell
