bb8feef2ee0c60051c46782d01607c2adacee05d
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-08-08 14:49:26 +0200

    there is no repository or stdout classes

--- lib/travis/worker/job.rb
+++ lib/travis/worker/job.rb
@@ -4,8 +4,6 @@ module Travis
       autoload :Base,       'travis/worker/job/base'
       autoload :Build,      'travis/worker/job/build'
       autoload :Config,     'travis/worker/job/config'
-      autoload :Repository, 'travis/worker/job/repository'
-      autoload :Stdout,     'travis/worker/job/stdout'
     end # Job
   end # Worker
 end # Travis
