8f537650f45ec9b3b735fdf2a27ff90ccdf34a86
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-09-27 21:33:03 +0200

    went to only an amqp worker, more work needed

--- lib/travis/worker/reporter.rb
+++ lib/travis/worker/reporter.rb
@@ -1,10 +1,28 @@
 module Travis
   module Worker
-    module Reporter
-      autoload :Base,  'travis/worker/reporter/base'
-      autoload :Amqp,  'travis/worker/reporter/amqp'
-      autoload :Http,  'travis/worker/reporter/http'
-      autoload :Queue, 'travis/worker/reporter/queue'
+    class Reporter
+      attr_reader :build, :messages, :connections
+
+      def initialize(channel)
+        @channel  = Travis::Worker.amqp_connection.create_channel
+        @exchange = Travis::Worker.amqp_connection.default_exchange
+      end
+
+      def on_start(data)
+        message(:start, data)
+      end
+
+      def on_update(data)
+        message(:update, data.merge(:incremental => true))
+      end
+
+      def on_finish(data)
+        message(:finish, data)
+      end
+
+      def message(type, data)
+        @exchange.publish(data[:log], :type => type.to_s, :routing_key => "reporting.progress", :arguments => { 'x-incremental' => !!data[:incremental] })
+      end
     end # Reporter
   end # Worker
 end # Travis
