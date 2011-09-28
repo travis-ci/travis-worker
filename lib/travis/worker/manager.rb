3db391f722501545d95615604886dd24ce597613
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-09-28 03:06:49 +0200

    Added Tomdoc docs to manager.rb, along with some minor corrections.

--- lib/travis/worker/manager.rb
+++ lib/travis/worker/manager.rb
@@ -1,49 +1,85 @@
 module Travis
   module Worker
+
+    # Public: The Worker manager, responsible for starting, monitoring,
+    # and stopping Worker instances.
     class Manager
 
-      attr_reader :worker_threads, :messaging_connection, :configuration
+      # Public: Returns an Array of threads of Worker instances.
+      attr_reader :worker_threads
 
-      def initialize(configuration, messaging_connection)
+      # Public: Returns the MessagingConnection used by the Manager.
+      attr_reader :messaging_connection
+
+      # Public: Initialize a Worker Manager.
+      #
+      # messaging_connection - A MessagingConnection instance.
+      # config - A Config to use for connection details (default: nil)
+      def initialize(messaging_connection, config = nil)
         @worker_threads = []
-        @configuration = configuration
         @messaging_connection = messaging_connection
+        @config = config
       end
 
+      # Public: Connects to the messaging broker and starts the workers.
+      #
+      # Returns the current instance of the MessagingConnection.
       def start
         messaging_connection.bind
         start_workers
+        self
       end
 
+      # Public: Disconnects from the messaging broker and stops all the workers.
+      #
+      # Returns the current instance of the MessagingConnection.
       def stop
         stop_workers
         messaging_connection.unbind
+        self
       end
 
 
       protected
 
+        # Internal: Starts new workers in new threads.
+        #
+        # Returns the current instance of the MessagingConnection.
         def start_workers
-          builds_queue = messaging_connection.builds_queue,
+          builds_queue = messaging_connection.builds_queue
           channel = messaging_connection.channel
 
-          configuration.vms.count.times do |num|
+          config.vms.count.times do |num|
             name = worker_name(num)
 
             puts "[boot] Starting #{name}"
 
             worker_threads << Worker.start_in_background(name, builds_queue, channel)
           end
+
+          self
         end
 
+        # Internal: Stops all current Worker instances, along with the thread
+        # they are running in.
         def stop_workers
-          workers.each { |worker| worker.value.stop_processing }
+          worker_threads.each do |thread|
+            thread.value.stop_processing
+            thread.stop
+          end
+        end
+
+
+      private
+
+        def config
+          @config ||= Travis::Worker.config
         end
 
         def worker_name(num)
-          "#{Travis::Worker.config.vms.name_prefix}-#{num}"
+          "#{config.vms.name_prefix}-#{num}"
         end
-
     end
+
   end
 end
