0248fa384848e894edc2f64994e9edcd8f9ec939
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-10-02 02:37:43 +0200

    updated the play scripts

--- play/enqueue.rb
+++ play/enqueue.rb
@@ -1,12 +1,61 @@
-require 'rubygems'
-require 'travis/worker'
-require 'fixtures/payloads'
+require 'hot_bunnies'
+require 'multi_json'
+require 'hashr'
 
-payload = INCOMING_PAYLOADS['build:gem-release']
 
-Travis::Worker.class_eval { @queue = 'builds' }
+class QueueTester
+  attr_reader :connection, :channel, :exchange, :reporting_queue
 
-1.times do
-  Resque.enqueue(Travis::Worker, payload)
+  def initialize
+    @connection = @channel = @exchange = @reporting_queue = nil
+  end
+
+  def start
+    connect
+    open_channel
+    connect_to_exchange
+  end
+
+  def stop
+    connection.close
+  end
+
+  def queue_job(payload)
+    exchange.publish(payload, :routing_key => 'builds')
+  end
+
+  private
+  def connect
+    @connection = HotBunnies.connect(:host => 'localhost')
+  end
+
+  def open_channel
+    @channel.close if @channel
+
+    @channel = connection.create_channel
+    @channel.prefetch = 1
+  end
+
+  def connect_to_exchange
+    @exchange = channel.exchange('', :type => :direct, :durable => true)
+  end
 end
 
+payload = {
+  'repository' => {
+    'slug' => 'svenfuchs/gem-release',
+  },
+  'build' => {
+    'id' => 1,
+    'commit' => '313f61b',
+    'branch' => 'master',
+    'config' => {
+      'rvm'    => '1.8.7',
+      'script' => 'rake'
+    }
+  }
+}
+
+queue_tester = QueueTester.new
+queue_tester.start
+queue_tester.queue_job(MultiJson.encode(payload))
