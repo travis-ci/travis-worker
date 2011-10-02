a7600790e3335433901a29a902d989cef9c0a72e
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2011-10-02 17:02:14 +0200

    Making tests at least run.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,7 +20,7 @@ class Test::Unit::TestCase
   attr_reader :shell
 
   def setup
-    @shell = Travis::Worker.shell = Mock::Shell.new
+    @shell = Mock::Shell.new
     Mocha::Mockery.instance.verify
   end
 
