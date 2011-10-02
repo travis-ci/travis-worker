13b369c36a120ad34b881fb2c8cd0a78c560287c
Sven Fuchs <svenfuchs@artweb-design.de>
2011-10-02 13:38:54 +0200

    make disks immutable by default

--- lib/travis/worker/cli/vagrant.rb
+++ lib/travis/worker/cli/vagrant.rb
@@ -13,6 +13,7 @@ module Travis
 
         desc 'update', 'Update the worker vms from a base box'
         method_option :env
+        method_option :immute,  :aliases => '-i', :type => :boolean, :default => true, :desc => 'Make all disks in the current vagrant environment immutable'
         method_option :reset,  :aliases => '-r', :type => :boolean, :default => false, :desc => 'Force reset on virtualbox settings and boxes'
         def update
           vbox.reset
@@ -20,7 +21,15 @@ module Travis
           # download
           add_box
           exit unless up
-          # immute_disk env
+          immute
+        end
+
+        desc 'immute', "Make all disks in the current vagrant environment immutable"
+        def immute
+          halt
+          uuids.each do |name, uuid|
+            immute_disk(name, uuid)
+          end
         end
 
         # desc 'remove', 'Remove the worker boxes'
@@ -53,12 +62,12 @@ module Travis
           #   run "wget http://files.vagrantup.com/#{from}.box" unless File.exists?("#{from}.box")
           # end
 
-          # def immute_disk(name)
-          #   run "VBoxManage modifyhd ~/.vagrant.d/boxes/#{name}/box-disk1.vmdk --type immutable"
-          # end
-
           def add_box
             run "vagrant box add #{env} #{base}"
+          end
+
+          def halt
+            run 'vagrant halt'
           end
 
           def up
@@ -73,10 +82,27 @@ module Travis
             run "vagrant destroy #{name}"
           end
 
-          # def uuid
-          #   vms = JSON.parse(File.read('.vagrant'))
-          #   vms['active']['base'] || raise("could not find base uuid in #{vms.inspect}")
-          # end
+          def uuid
+            vms = JSON.parse(File.read('.vagrant'))
+            vms['active']['base'] || raise("could not find base uuid in #{vms.inspect}")
+          end
+
+          def uuids
+            # "travis-worker_1317520507" {0ea36f25-89a2-4a79-8e6a-d6f6a4450b8f}
+            # "travis-worker_1317520537" {b567d985-2c8a-4f8f-bcd3-be2d4b60e764}
+            `VBoxManage list vms`.split("\n").map do |vm|
+              vm =~ /"(.*)" {(.*)}/
+              [$1, $2]
+            end
+          end
+
+          def immute_disk(name, uuid)
+            run <<-sh
+              VBoxManage storageattach #{uuid} --storagectl "SATA Controller" --port 0 --device 0 --medium none
+              VBoxManage modifyhd ~/VirtualBox\\ VMs/#{name}/box-disk1.vmdk --type immutable
+              VBoxManage storageattach #{uuid} --storagectl "SATA Controller" --port 0 --device 0 --medium ~/VirtualBox\\ VMs/#{name}/box-disk1.vmdk --type hdd
+            sh
+          end
       end
     end
   end
