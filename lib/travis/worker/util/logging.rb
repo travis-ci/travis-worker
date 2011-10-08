ec3bbef45d09287bc1493aee3cfe6ee10a76a7f6
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-10-08 16:45:23 +0200

    improved and fixed the loggin mixin a little

--- lib/travis/worker/util/logging.rb
+++ lib/travis/worker/util/logging.rb
@@ -2,8 +2,9 @@ module Travis
   module Worker
     module Util
       module Logging
-        def announce(message)
-          message.split("\n").each do |msg|
+        def announce(messages)
+          messages = messages.split("\n") if messages.is_a?(String)
+          messages.each do |msg|
             puts "#{format_logging_header}#{msg}"
           end
         end
@@ -16,13 +17,21 @@ module Travis
         private
           def format_logging_header
             if header = Thread.current[:logging_header]
-              "[#{header}] "
+              "[#{yellow(header)}] "
             elsif respond_to?(:logging_header)
-              "[#{self.logging_header}] "
+              "[#{yellow(self.logging_header)}] "
             else
               ""
             end
           end
+
+          def colorize(text, color_code)
+            "#{color_code}#{text}\e[0m"
+          end
+
+          def red(text);    colorize(text, "\e[31m"); end
+          def green(text);  colorize(text, "\e[32m"); end
+          def yellow(text); colorize(text, "\e[33m"); end
       end
     end
   end
