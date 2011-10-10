e5226b125f73c8d6912cb890780297f490678e8d
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-10-10 21:15:48 +0200

    pass in a logger to Session

--- lib/travis/worker/virtual_machine/virtual_box.rb
+++ lib/travis/worker/virtual_machine/virtual_box.rb
@@ -110,7 +110,8 @@ module Travis
             :username => 'vagrant',
             :private_key_path => File.expand_path('keys/vagrant'),
             :buffer => Travis::Worker.config.shell.buffer,
-            :timeouts => Travis::Worker.config.timeouts
+            :timeouts => Travis::Worker.config.timeouts,
+            :logger => Util::Logging::Logger.new(name)
           )
         end
 
