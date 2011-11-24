e515c83175436d1bf5926dd2a5fc173af3403b1f
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-24 13:45:17 +0100

    improve autoload gauntlet

--- lib/autoload_gauntlet.rb
+++ lib/autoload_gauntlet.rb
@@ -2,14 +2,15 @@ require 'travis/worker'
 require 'travis/build'
 require 'faraday'
 
-module AutoloadHelper
-  def load_autoloaded_constants(recursive = false)
+module ConstantLoadHelper
+  def load_constants(loaded = [])
     constants.each do |name|
-      next unless autoload?(name)
+      next if loaded.include?(name)
+      loaded << name
       const = const_get(name)
-      if recursive
-        const.extend(AutoloadHelper)
-        const.load_autoloaded_constants(true)
+      if const.is_a?(Class) || const.is_a?(Module)
+        const.extend(ConstantLoadHelper)
+        const.load_constants(loaded)
       end
     end
   end
@@ -18,6 +19,6 @@ end
 targets = [Travis::Worker, Travis::Build, Faraday]
 
 targets.each do |target|
-  target.extend(AutoloadHelper)
-  target.load_autoloaded_constants(true)
+  target.extend(ConstantLoadHelper)
+  target.load_constants
 end
