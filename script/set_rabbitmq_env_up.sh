8b42e7588869acfc573dcf54bb733227c7a88c1d
Michael S. Klishin <michael@novemberain.com>
2012-01-19 21:05:37 +0400

    Oops

--- script/set_rabbitmq_env_up.sh
+++ script/set_rabbitmq_env_up.sh
@@ -3,5 +3,5 @@
 rabbitmqctl add_vhost "travis.development"
 rabbitmqctl add_user travisci_worker travisci_worker_password
 
-rabbitmqctl set_permissions -p "travisci.development" travis_worker ".*" ".*" ".*"
+rabbitmqctl set_permissions -p "travisci.development" travisci_worker ".*" ".*" ".*"
 rabbitmqctl set_permissions -p "travisci.development" guest         ".*" ".*" ".*"
