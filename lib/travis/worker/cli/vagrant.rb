aa28cf44f09e97e6325f949819984d1a1d8b7491
Sven Fuchs <svenfuchs@artweb-design.de>
2012-02-25 17:25:07 +0100

    change the behaviour of travis:vms:update so that --download replaces --local and takes either nothing, a local path or a url (default changes to --no-download)

--- lib/travis/worker/cli/vagrant.rb
+++ lib/travis/worker/cli/vagrant.rb
@@ -11,38 +11,27 @@ module Travis
 
         include Cli
 
-        desc 'update', 'Update the worker vms from a base box'
-        method_option :env
+        desc 'update [BOX]', 'Update the worker vms from a base box (BOX defaults to Travis::Worker.config.env)'
         method_option :immute,   :aliases => '-i', :type => :boolean, :default => false, :desc => 'Make all disks in the current vagrant environment immutable'
         method_option :snapshot, :aliases => '-s', :type => :boolean, :default => true,  :desc => 'Take online snapshots of all vms'
         method_option :reset,    :aliases => '-r', :type => :boolean, :default => false, :desc => 'Force reset on virtualbox settings and boxes'
-        method_option :local,    :aliases => '-l', :type => :boolean, :default => true,  :desc => 'Copy the packaged base box from ../travis-boxes/boxes to ./boxes otherwise upload them to s3'
+        method_option :download, :aliases => '-d', :type => :string,  :default => false, :desc => 'Copy/download the base box from the given path, storage or URL (will use file.travis.org if -d is given without a string)'
 
-        # TODO i think the options should work slightly different here:
-        #
-        # I think it should just use whatever ./boxes/[...].box is there. Then there
-        # should be an option --download (which defaults to false) to indicate that
-        # we want to download/overwrite to ./boxes. It can default to ../travis-boxes/boxes
-        # but also be overwritten with either a local path or a url.
+        def update(box = Travis::Worker.config.env)
+          self.box = box
 
-        def update
           vbox.reset if options[:reset]
-          remove_base_box
-          options[:local] ? local : download
+          download if download?
           add_box
           exit unless up
           halt
+        rescue => e
+          puts e.message
         end
 
-        # desc 'remove', 'Remove the worker boxes'
-        # def remove
-        #   1.upto(config.count) do |num|
-        #     destroy "worker-#{num}"
-        #     remove_box "worker-#{num}"
-        #   end
-        # end
-
         protected
+
+          attr_accessor :box
 
           def vbox
             @vbox ||= Vbox.new('', options)
@@ -52,30 +41,37 @@ module Travis
             self.class.config
           end
 
-          def env
-            options['env'] || Travis::Worker.config.env
+          def base_box
+            "boxes/travis-#{box}.box"
           end
 
-          def base
-            "boxes/travis-#{env}.box"
-          end
-
-          def remove_base_box
-            run "rm -rf boxes/travis-#{env}.box"
-          end
-
-          def local
-            run "cp ../travis-boxes/boxes/travis-#{env}.box boxes" # TODOdon't copy if files are identical
+          def download?
+            !!options[:download] || !File.exists?(base_box)
           end
 
           def download
-            unless File.exists?("boxes/travis-#{env}.box")
-              run "wget http://files.travis-ci.org/boxes/provisioned/travis-#{env}.box -P boxes"
+            download_failed! unless run(download_command)
+          end
+
+          def download_command
+            source =~ /^http:/ ? "wget #{source} -P boxes" : "cp #{source} boxes"
+          end
+
+          def download_failed!
+            raise "The download command #{download_command} failed, terminating ..."
+          end
+
+          def source
+            case options[:download]
+            when 'download', NilClass
+              "http://files.travis-ci.org/boxes/provisioned/travis-#{box}.box"
+            else
+              options[:download]
             end
           end
 
           def add_box
-            run "vagrant box add travis-#{env} #{base}"
+            run "vagrant box add travis-#{box} #{base_box}"
           end
 
           def up
