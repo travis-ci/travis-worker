2c3860fd63c2f1b6f66fcaefb25ff728fa5ed4eb
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-08-07 19:55:34 +0200

    this is a crud version of the reaper, but @rkh helped explain the blocking issues we are experiencing

--- lib/travis/worker/reaper.rb
+++ lib/travis/worker/reaper.rb
@@ -10,15 +10,20 @@ module Travis
       end
 
       def self.live_or_let_die(vm)
-        info "monitoring #{vm.name} (pid #{vm.vm_pid}) so that it doesn't exceed 45mins"
-        Timeout.timeout(45 * 60) do
-          yield
+        info "monitoring #{vm.name} so that it doesn't exceed 45mins"
+        done = false
+        stupid_timeout = Thread.new do
+          sleep 45 * 60
+          unless done
+            `kill #{vm.vm_pid}`
+            info "#{vm.name} (pid #{vm.vm_pid}) forcefully killed"
+            sleep 5
+          end
         end
-      rescue Timeout::Error => e
-        `kill #{vm.vm_pid}`
-        info "#{vm.name} (pid #{vm.vm_pid}) forcefully killed"
-        sleep 5
-        true
+        result = yield
+        done = true
+        stupid_timeout.kill rescue nil
+        result
       end
     end
   end
