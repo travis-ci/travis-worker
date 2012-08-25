125877a5884dddb288a511a0d101a787cc7b07e9
Michael S. Klishin <michael@novemberain.com>
2012-08-25 14:07:15 +0400

    Make CI env username configurable via an ENV var

--- lib/travis/worker/virtual_machine/virtual_box.rb
+++ lib/travis/worker/virtual_machine/virtual_box.rb
@@ -109,7 +109,7 @@ module Travis
           @shell ||= Shell::Session.new(name,
             :host => '127.0.0.1',
             :port => ssh_port,
-            :username => 'travis',
+            :username => ENV.fetch("TRAVIS_CI_ENV_USERNAME", 'travis'),
             :private_key_path => File.expand_path('keys/vagrant'),
             :buffer => Travis::Worker.config.shell.buffer,
             :timeouts => Travis::Worker.config.timeouts
