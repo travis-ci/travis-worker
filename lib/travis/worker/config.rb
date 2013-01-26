0c1fb0f7dc7a3c7820a87e3a9cec935c4554e100
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-01-27 02:47:07 +1300

    remove the default timeout info, and bump the log timeout to 10minutes, and set the default shell buffer to 0.5

--- lib/travis/worker/config.rb
+++ lib/travis/worker/config.rb
@@ -29,10 +29,10 @@ module Travis
              :log_level => :info,
              :queue     => 'builds.common',
              :logging_channel => 'reporting.jobs.logs',
-             :shell     => { :buffer => 0 },
-             :timeouts  => { :hard_limit => 3000, :before_install => 300, :install => 300, :before_script => 300, :script => 600, :after_script => 300, :after_success => 300, :after_failure => 300, :default => 180 },
+             :shell     => { :buffer => 0.5 },
+             :timeouts  => { :hard_limit => 3000 },
              :vms       => { :provider => 'virtual_box', :count => 1, :_include => Vms },
-             :limits    => { :log_length => 4, :last_flushed => 5 }
+             :limits    => { :log_length => 4, :last_flushed => 10 }
 
       def name
         @name ||= host.split('.').first
