03fa43b59f6a1b01ed8ed7c23475f3e76654eef5
Henrik Hodne <me@henrikhodne.com>
2013-03-27 20:37:05 -0500

    Update sshjr connector

--- lib/travis/worker/ssh/connector/sshjr.rb
+++ lib/travis/worker/ssh/connector/sshjr.rb
@@ -21,59 +21,15 @@ module Travis
             @client.close if open?
           end
 
-          def exec(command, buffer)
+          def exec(command, buffer, &block)
             connect unless open?
 
-            exit_code = nil
-
-            session = @client.start_session
-            session.allocate_default_pty
-            command = session.exec("/bin/bash --login -c #{Shellwords.escape(command)}")
-            output = command.input_stream.to_io # In Java, input is output, apparently.
-            error = command.error_stream.to_io
-
-            data_threads = []
-            data_mutex = Mutex.new
-
-            data_threads << Thread.new(output, buffer) do |output, buffer|
-              output.each_char do |char|
-                data_mutex.synchronize do
-                  buffer << char
-                end
-              end
+            command = @client.exec_with_pty("/bin/bash --login -c #{Shellwords.escape(command)}")
+            command.on_output do |str|
+              buffer << str
             end
 
-            data_threads << Thread.new(error, buffer) do |error, buffer|
-              error.each_char do |char|
-                data_mutex.synchronize do
-                  buffer << char
-                end
-              end
-            end
-
-            loop do
-              if block_given?
-                early_exit = yield
-                exit_code = command.exit_status
-                if (early_exit || !!exit_code)
-                  command.close
-                  session.close
-                end
-                sleep(0.5)
-              else
-                sleep(1)
-                exit_code = command.exit_status
-                if exit_code
-                  command.close
-                  session.close
-                  break
-                end
-              end
-            end
-
-            data_threads.map(&:exit)
-
-            exit_code
+            command.process(&block)
           end
 
           private
