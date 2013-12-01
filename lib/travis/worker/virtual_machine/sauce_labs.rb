ae95f1e4d6f08c82d9fb9f2f235d825db43b4d05
Henrik Hodne <henrik@hodne.io>
2013-12-01 02:14:02 -0600

    sauce_labs: Make all private methods private
    Knowing what methods are used outside of the class makes it easier to
    refactor.

--- lib/travis/worker/virtual_machine/sauce_labs.rb
+++ lib/travis/worker/virtual_machine/sauce_labs.rb
@@ -30,35 +30,8 @@ module Travis
           @name = name
         end
 
-        def connection
-          @connection ||= Travis::SaucelabsAPI.new(Travis::Worker.config.sauce_labs.api_endpoint)
-        end
-
-        def create_server(opts = {})
-          retryable(:tries => 3) do
-            destroy_duplicate_server(hostname)
-            create_new_server
-          end
-        end
-
-        def create_new_server
-          @server = start_server
-          info "Booting #{hostname} (#{ip_address})"
-          instrument do
-            Fog.wait_for(240, 3) do
-              vm_ready?(@server)
-            end
-          end
-        rescue Timeout::Error, Fog::Errors::TimeoutError => e
-          if @server
-            error "Sauce Labs VM would not boot within 240 seconds: id=#{@server["instance_id"]}"
-          end
-          Metriks.meter("worker.vm.provider.saucelabs.boot.timeout").mark
-          raise
-        rescue StandardError => e
-          Metriks.meter("worker.vm.provider.saucelabs.boot.error").mark
-          error "Booting a Sauce Labs VM failed with the following error: #{e.inspect}"
-          raise
+        def prepare
+          info "Sauce Labs API adapter prepared"
         end
 
         def session
@@ -79,12 +52,45 @@ module Travis
           create_server(opts)
           yield
         ensure
-          session.close
+          session.close if session
           destroy_server
         end
 
         def full_name
           "#{Travis::Worker.config.host}:travis-#{name}"
+        end
+
+        private
+
+        def api
+          @api ||= Travis::SaucelabsAPI.new(Travis::Worker.config.sauce_labs.api_endpoint)
+        end
+
+        def create_server(opts = {})
+          retryable(:tries => 3) do
+            destroy_duplicate_server(hostname)
+            create_new_server
+          end
+        end
+
+        def create_new_server
+          @server = start_server
+          info "Booting #{hostname} (#{ip_address})"
+          instrument do
+            Fog.wait_for(240, 3) do
+              vm_ready?(@server)
+            end
+          end
+        rescue Timeout::Error, Fog::Errors::TimeoutError
+          if @server
+            error "Sauce Labs VM would not boot within 240 seconds: id=#{@server["instance_id"]}"
+          end
+          Metriks.meter("worker.vm.provider.saucelabs.boot.timeout").mark
+          raise
+        rescue  => e
+          Metriks.meter("worker.vm.provider.saucelabs.boot.error").mark
+          error "Booting a Sauce Labs VM failed with the following error: #{e.inspect}"
+          raise
         end
 
         def hostname
@@ -96,8 +102,8 @@ module Travis
         end
 
         def start_server
-          instance_id = connection.start_instance({ hostname: hostname }, 'ichef-travis-osx8-latest')['instance_id']
-          connection.instance_info(instance_id)
+          instance_id = api.start_instance({ hostname: hostname }, 'ichef-travis-osx8-latest')['instance_id']
+          api.instance_info(instance_id)
         end
 
         def destroy_server(opts = {})
@@ -108,21 +114,15 @@ module Travis
         end
 
         def destroy_duplicate_server(hostname)
-          instance_ids = connection.list_instances['instances']
-          instances = instance_ids.map { |instance_id| connection.instance_info(instance_id) }
-          instance = instances.detect do |instance|
-            name = instance['extra_info']['hostname'] if instance && instance['extra_info']
+          instance_ids = api.list_instances['instances']
+          instances = instance_ids.map { |instance_id| api.instance_info(instance_id) }
+          instances = instances.compact.select do |instance|
+            name = instance['extra_info']['hostname'] if instance['extra_info']
             name == hostname
           end
 
-          destroy_vm(instance) if instance
+          instances.each { |instance| destroy_vm(instance) }
         end
-
-        def prepare
-          info "Sauce Labs API adapter prepared"
-        end
-
-        private
 
         def instrument
           info "Provisioning a Sauce Labs VM"
@@ -134,12 +134,12 @@ module Travis
         def destroy_vm(vm)
           info "destroying the VM"
           retryable(tries: 3) do
-            connection.kill_instance(vm['instance_id'])
+            api.kill_instance(vm['instance_id'])
           end
         end
 
         def vm_ready?(vm)
-          socket = TCPSocket.new(vm['private_ip'], 3422)
+          socket = TCPSocket.new(ip_address, 3422)
           true
         rescue StandardError
           false
