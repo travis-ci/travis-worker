e3f95477ee55bb1324e76c5f990e3f354a404937
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-06-05 15:56:01 +1200

    fix all the rspec specs (only 1)

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -54,8 +54,8 @@ describe Travis::Worker::Config do
 
   it 'vms includes the Vms module' do
     Travis::Worker::Config.any_instance.stubs(:read_yml).returns({ 'vms' => { 'count' => 5 } })
-    config.vms.meta_class.included_modules.include?(Travis::Worker::Config::Vms).should be_true
-    config.vms.count.should eql 5
+    expect(config.vms.meta_class.included_modules.include?(Travis::Worker::Config::Vms)).to eq(true)
+    expect(config.vms.count).to eql(5)
   end
 
   describe :defaults do
