164ba76e5dfd41d38ff82d80e3934632e77488fa
Henrik Hodne <henrik@hodne.io>
2014-11-24 23:37:40 +0100

    Add Celluloid mocking workaround
    This workaround is taken from celluloid/celluloid#26 and is needed to avoid
    infinite loops when using mocking and Celluloid.

---
+++ spec/support/actor_mocking.rb
@@ -0,0 +1,11 @@
+# From https://github.com/celluloid/celluloid/issues/26#issuecomment-4968921
+RSpec.configuration.before(:each) do
+  class Celluloid::ActorProxy
+    unless @rspec_compatible
+      @rspec_compatible = true
+      if respond_to?(:should_receive)
+        undef_method :should_receive
+      end
+    end
+  end
+end
