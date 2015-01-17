c82269d29a6610aedfb0ca37393dbeb6f97d4b65
Dan Buch <dan@travis-ci.org>
2015-01-17 15:19:46 +0100

    Use existing request stubbing to avoid conflicts

--- spec/application/http_heart_spec.rb
+++ spec/application/http_heart_spec.rb
@@ -5,20 +5,15 @@ describe Travis::Worker::Application::HTTPHeart do
   let(:state) { { state: :up } }
   let(:heart) { Travis::Worker::Application::HTTPHeart.new('http://example.org', shutdown_block) }
   let(:shutdown_block) { ->(*) { state[:state] = :down } }
-  let(:conn) { Excon.new('http://example.org', mock: true) }
-
-  before :each do
-    heart.stubs(:conn).returns(conn)
-  end
 
   it 'stays up if the expected_state is up' do
-    Excon.stub({}, {body: '{"expected_state":"up"}', status: 200})
+    stub_request(:post, 'http://example.org').to_return(body: '{"expected_state":"up"}', status: 200)
     heart.beat
     expect(state[:state]).to eq(:up)
   end
 
   it 'shuts down if the expected_state is down' do
-    Excon.stub({}, {body: '{"expected_state":"down"}', status: 200})
+    stub_request(:post, 'http://example.org').to_return(body: '{"expected_state":"down"}', status: 200)
     heart.beat
     expect(state[:state]).to eq(:down)
   end
